0d14cc002adc2a62fce756eb401b269925daed28
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-11-02 18:03:24 +0100

    reworked the logic to link other pages to be rendered

--- src/index.js
+++ src/index.js
@@ -1,35 +1,88 @@
-import homePage from "./home"
+import './style.css'
+import gitIcon from '../src/RestaurantImages/github-logo.svg'
+import home from "./home"
 
 const render = (() => {
-    function createElement(element){
-        return document.createElement(element);
-    };
 
     function renderHeader() {
-      const header = createElement('div');
+      const header = document.createElement('div');
+      const title = document.createElement('h1');
       header.setAttribute('id','header');
+      title.textContent = 'Lights';
+    //   Appends
       document.body.appendChild(header);
+      header.appendChild(title);
     };
     function renderNav(){
-        const nav = createElement('nav');
+        const nav = document.createElement('nav');
         nav.setAttribute('id','nav');
         document.body.appendChild(nav);
+        const navItems = ['Home','Menu','Contact'];
+        // Menu items
+        for (let i = 0; i < navItems.length; i += 1) {
+          const navItem = document.createElement('a');
+          navItem.id = navItems[i];
+          navItem.classList.add('nav-link');
+          navItem.setAttribute('href', '#');
+          navItem.textContent = navItems[i];
+          nav.appendChild(navItem);
+        }
     };
+// }
     function renderContent(){
-        const section = createElement('section');
+        const section = document.createElement('section');
         section.setAttribute('id','content');
         document.body.appendChild(section)
     };
     function renderFooter(){
-        const footer = createElement('footer');
-        footer.setAttribute('id','footer');
+        const footer = document.createElement('footer');
+        const p = document.createElement('p');
+        const link = document.createElement('a');
+        const logo = document.createElement('img');
+        p.textContent = 'Copyright © 2021 AminuMado';
+        link.setAttribute('href','https://github.com/AminuMado');
+        link.setAttribute('target','blank');
+        logo.setAttribute('src',gitIcon);
+        footer.classList.add('footer');
+        logo.classList.add('github-logo');
+        // Appends
+        link.appendChild(logo);
+        footer.appendChild(p);
+        footer.appendChild(link);
         document.body.appendChild(footer);
     };
+    function buttonsHandler() {
+        const menuItems = document.querySelectorAll('#nav a');
+        const menuItemHome = menuItems[0];
+        const menuItemMenu = menuItems[1];
+        const menuItemContacts = menuItems[2];
+    
+        function removeCurrentClass() {
+          menuItems.forEach((element) => {
+            element.classList.remove('current');
+          });
+        }
+    
+        document.addEventListener('click', (event) => {
+          removeCurrentClass();
+          if (event.target.id === 'Home'){
+            home.render();
+            menuItemHome.classList.add('current');
+          } else if (event.target.id === 'Menu'){
+            menu.render();
+            menuItemMenu.classList.add('current');
+          } else if (event.target.id === 'Contact'){
+            contact.render();
+            menuItemContacts.classList.add('current');
+          };
+    });
+}
     function start(){
         renderHeader();
         renderNav();
         renderContent();
         renderFooter();
+        buttonsHandler();
     };
     return {start}
   })();
